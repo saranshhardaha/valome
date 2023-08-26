bcb507631263fd593c774bd80580b6c58906e377
Saransh <hellosaranshh@gmail.com>
2023-08-26 19:52:28 +0530

    Update Packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    theme: {
-    extend: {},
-  },
-  variants: {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
     extend: {},
   },
   plugins: [],
